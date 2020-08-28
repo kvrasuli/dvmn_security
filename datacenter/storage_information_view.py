4a225d545bb1d6f1dd9257c42050c67f57ff7069
Kirill Rasuli <rasulikv@gmail.com>
2020-08-29 01:49:12 +0300

    Display a list of people in vault

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,20 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits_query = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in non_closed_visits_query:
+        non_closed_visits.append(
+            {
+                "who_entered": visit.passcard.owner_name,
+                "entered_at": visit.entered_at,
+                "duration": visit.get_duration(),
+            }
+        )
 
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
-    ]
     context = {
-        "non_closed_visits": non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": non_closed_visits,  # незакрытые посещения
     }
     return render(request, 'storage_information.html', context)
