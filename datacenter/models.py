26db06b9ff60c48e6e35348fc4318e0b5c820e79
Kirill Rasuli <rasulikv@gmail.com>
2020-08-29 01:45:56 +0300

    Implement funcs to calc visit duration

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -19,9 +20,19 @@ class Visit(models.Model):
     entered_at = models.DateTimeField()
     leaved_at = models.DateTimeField(null=True)
 
+    def get_duration(self):
+        duration_seconds = (localtime() - self.entered_at).total_seconds()
+        return self.format_duration(duration_seconds)
+
+    @staticmethod
+    def format_duration(duration):
+        hours = duration // 3600
+        minutes = (duration % 3600) // 60
+        return f'{int(hours)}h {int(minutes)}m'
+
     def __str__(self):
         return "{user} entered at {entered} {leaved}".format(
             user=self.passcard.owner_name,
             entered=self.entered_at,
-            leaved= "leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
+            leaved="leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
         )
